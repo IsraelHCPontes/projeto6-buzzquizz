b5108ae8457d72443859cdf5cd673c1815e69e32
cazumba22 <israelhcpontes@gmail.com>
2022-07-02 00:42:43 -0300

    FRONTE_NIVEIS_3.2

--- js/script.js
+++ js/script.js
@@ -71,12 +71,101 @@ function editaNivel(elemento) {
     elemento.parentNode.classList.toggle('toggleNivel')
     elemento.parentNode.querySelector('.caixaNivel').classList.toggle('opacidade')
     elemento.parentNode.querySelector('.buttonEditar').classList.toggle('invisivel')
-    
 }
-   
-   // função cria quantidade de niveis passadas 
-   function renderizaNivel(elemento){
-    let quantidadeNivel = document.querySelector('.nivelQuiz').value
+
+let botao_criar_Pergunta = document.querySelector(".prmeiraParte .botao1")
+function  renderizaPergunta(){
+    console.log('passou pergunta')
+    quantidadePergunta = document.querySelector('.perguntasQuiz').value
+    document.querySelector('.perguntas').innerHTML = ""
+    for (let i = 0; i < quantidadePergunta; i++) {
+        newPergunta = i + 1
+        template_caixaPergunta = ` 
+        <div class="caixa1">
+        <div class="pergunta1">
+            <div class="pergunta01">
+            <h1>Pergunta ${newPergunta}</h1>
+            <img src="./images/img1.svg"/>
+            </div>
+            <!--Fecha caixa pergunta 01-->
+            <input
+                class="textoDaPergunta"
+                type="text"
+                placeholder="Texto da pergunta"
+            />
+            <input
+                class="corDoFundo"
+                type="text"
+                placeholder="Cor de fundo da pergunta"
+            />
+        </div>
+        <!--Fecha caixa pergunta 1-->
+
+        <div class="respostaCorreta">
+            <h1>Resposta Correta</h1>
+            <input
+                class="RespostaCorreta"
+                type="text"
+                placeholder="Resposta Correta"
+            />
+            <input
+                class="urlDaImagem"
+                type="text"
+                placeholder="URL da imagem"
+            />
+        </div>
+        <!--Fecha caixa Resposta Correta-->
+        <div class="respostasIncorretas">
+            <h1>Respostas incorretas</h1>
+            <input
+                class="respostaIncorreta"
+                type="text"
+                placeholder="Resposta incorreta 1"
+            />
+            <input
+                class="urlDaImagem"
+                type="text"
+                placeholder="URL da imagem 1"
+            />
+            <input
+                class="respostaIncorreta"
+                type="text"
+                placeholder="Resposta incorreta 2"
+            />
+            <input
+                class="urlDaImagem"
+                type="text"
+                placeholder="URL da imagem 2"
+            />
+            <input
+                class="respostaIncorreta"
+                type="text"
+                placeholder="Resposta incorreta 3"
+            />
+            <input
+                class="urlDaImagem"
+                type="text"
+                placeholder="URL da imagem 3"
+            />
+        </div>
+        <!--Fecha caixa Respostas Incorretas-->
+    </div>
+    <!--Fecha caixa caixa 1-->
+                   `
+        document.querySelector('.perguntas').innerHTML += template_caixaPergunta
+    }
+}
+botao_criar_pergunta.addEventListener("click", renderiza)
+
+function renderiza(){
+    renderizaPergunta()
+    renderizaNivel() 
+}
+
+// função cria quantidade de niveis passadas 
+function renderizaNivel(elemento) {
+    quantidadeNivel = document.querySelector('.nivelQuiz').value
+    document.querySelector('.niveis').innerHTML = ""
     console.log(quantidadeNivel)
     for(let i = 0; i < quantidadeNivel; i++){   
        newNivel = i + 1
@@ -110,35 +199,33 @@ function editaNivel(elemento) {
                        </div>
                    </div>
                    `
-       document.querySelector('.niveis').innerHTML += template_caixaNivel
-     }
-   }
-   
-   //tratamento de paramentros 
-let botao_finalizar_quizz = document.querySelector(".terceiraParte").querySelector(".botaoNivel") 
-
-function info_basica_Nivel() {
-    /*
-     const titulo = document.querySelector(".terceiraParte").querySelector(".tituloNivel").value
-     const caracteres = titulo.split("")
-           
-     const url_niveis = document.querySelector(".terceiraParte").querySelector(".urlNivel").value
-           
-     const descricaoNivel_Nivel = document.querySelector(".terceiraParte").querySelector(".descricaoNivel").value
-           
-     const porcentagemAcerto_Nivel = document.querySelector(".terceiraParte").querySelector(".porcentagemAcerto").value
-           
-   
-     if(caracteres.length < 20 || descricaoNivel_Nivel < 30 || porcentagemAcerto_Nivel != 0){
-        alert("preencher os dados corretamente.")
-     }
-    try {
-      let url = new URL(url_niveis)
-        } catch(err) {
-        alert("A URL da imagem esta incorreta")
-        }*/
-
-    console.log("foi!!")
+        document.querySelector('.niveis').innerHTML += template_caixaNivel
+    }
 }
 
+//tratamento de paramentros 
+
+let botao_finalizar_quizz = document.querySelector(".terceiraParte .botaoNivel")
+function info_basica_Nivel() {
+    console.log(quantidadeNivel)
+    titulo = document.querySelectorAll(".terceiraParte .tituloNivel")
+    url = document.querySelectorAll(".terceiraParte .urlNivel")
+    descricaoNivel = document.querySelectorAll(".terceiraParte .descricaoNivel")
+    porcentagemAcerto = document.querySelectorAll(".terceiraParte .porcentagemAcerto")
+
+    for(let i = 0; i < titulo.length; i++){
+        console.log(titulo[i].value.length ,descricaoNivel[i].value.length , porcentagemAcerto[i].value)
+        if(titulo[i].value.length < 20 || descricaoNivel[i].value.length < 30 || porcentagemAcerto[i].value < 0){
+            alert("preencher os dados corretamente.")
+         }
+        try {let url = new URL(url[i].value)       
+             } catch(err) {
+            alert("A URL da imagem esta incorreta")
+            }
+    }
+          
+}
 botao_finalizar_quizz.addEventListener("click", info_basica_Nivel)
+   
+
+ 
