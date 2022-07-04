50f1ec60bfdc1c811cf0918259bdc3148e1826d1
THIAGO-BARBOSA-ARAUJO <thiagobarbosa.araujo@outlook.com>
2022-07-04 12:16:11 -0300

    projeto atualizado

--- js/script.js
+++ js/script.js
@@ -10,10 +10,11 @@ let botao_voltar_home = document.querySelector(".quartaParte").querySelector(".b
 
 let botao_criar_quizz = document.querySelector(".primeiraTela").querySelector(".botaoCriarQuizz") 
 
+let botao_finalizar_quizz = document.querySelector(".botaoNivel")
 
 function enviaQuizz(){
-    let titulo, cor_de_fundo, resposta_correta, url_img, resposta_incorreta
-    const caixa1 = document.querySelectorAll(".caixa1")
+    // let titulo, cor_de_fundo, resposta_correta, url_img, resposta_incorreta
+    const caixa1 = document.querySelectorAll(".containerPerguntas")
     const obj = {
         title: document.querySelector(".tituloQuiz").value,
         image: document.querySelector(".imagemQuiz").value,
@@ -25,7 +26,7 @@ function enviaQuizz(){
         const texto_pergunta = element.querySelector(".textoDaPergunta").value
         const cor_fundo = element.querySelector(".corDoFundo").value
         const resp_correta = element.querySelector(".RespostaCorreta").value
-        const url_img_correta = element.querySelector(".urlDaImagemCorreta").value
+        const url_img_correta = element.querySelector(".urlDaImagemCorreta").value // falar para o israel alterar no dele
         const obj_pergunta = {
             title: texto_pergunta,
 			color: cor_fundo,
@@ -84,8 +85,12 @@ function enviaQuizz(){
         }
         
     })
-    
+
+    document.querySelector('.terceiraParte').classList.add('desligado')
+    document.querySelector('.quartaParte').classList.remove('desligado')
 }
+
+botao_finalizar_quizz.addEventListener("click", enviaQuizz)
 
 /*função para enviar o quiz pro servidor */
 
@@ -158,108 +163,6 @@ atualizar_seus_quizes()
 /* tela Crie suas perguntas */
 
 
-/*função para enviar o quiz pro servidor */
-
-function enviaQuizz(){
-    let titulo, cor_de_fundo, resposta_correta, url_img, resposta_incorreta
-    const caixa1 = document.querySelectorAll(".caixa1")
-    const obj = {
-        title: document.querySelector(".tituloQuiz").value,
-        image: document.querySelector(".imagemQuiz").value,
-        questions:[],
-        levels:[]
-    }
-    for(let i = 0; i < caixa1.length; i++){
-        const element = caixa1[i]
-        const texto_pergunta = element.querySelector(".textoDaPergunta").value
-        const cor_fundo = element.querySelector(".corDoFundo").value
-        const resp_correta = element.querySelector(".RespostaCorreta").value
-        const url_img_correta = element.querySelector(".urlDaImagemCorreta").value
-        const obj_pergunta = {
-            title: texto_pergunta,
-			color: cor_fundo,
-            answers: [
-				{
-					text: resp_correta,
-					image: url_img_correta,
-					isCorrectAnswer: true
-				},
-			]
-        }
-
-        const texto_resp_incorretas = element.querySelectorAll(".respostaIncorreta")
-        const url_resp_incorretas = element.querySelectorAll(".urlDaImagem")
-        for(let i = 0; i <texto_resp_incorretas.length; i++){
-            const texto_resp_incorreta = texto_resp_incorretas[i].value
-            const url_resp_incorreta = url_resp_incorretas[i].value
-
-            const obj_resp = {
-                text: texto_resp_incorreta,
-                image: url_resp_incorreta,
-                isCorrectAnswer: false
-            }
-            obj_pergunta.answers.push(obj_resp)
-        }
-        obj.questions.push(obj_pergunta)
-
-    }
-    const niveis = document.querySelectorAll(".caixaNivel")
-    for(let i = 0; i < niveis.length; i++){
-        const element = niveis[i]
-        const titulo_nivel = element.querySelector(".tituloNivel").value
-        const porcentagem_acerto = element.querySelector(".porcentagemAcerto").value
-        const url_nivel = element.querySelector(".urlNivel").value
-        const descricao_nivel = element.querySelector(".descricaoNivel").value
-        
-        const obj_nivel = {
-            title: titulo_nivel,
-			image: url_nivel,
-			text: descricao_nivel,
-			minValue: Number(porcentagem_acerto)
-        }
-        obj.levels.push(obj_nivel)
-    }
-
-    /*pegar os titulos das perguntas */
-    //console.log(obj)
-
-    const promessa = axios.post("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes", obj)
-    promessa.then((res)=>{
-        const quizes_usuarios = JSON.parse(localStorage.getItem("quizes_usuarios"))
-        if(quizes_usuarios){
-            localStorage.setItem("quizes_usuarios", JSON.stringify([res.data.id, ...quizes_usuarios]))
-        }else{
-            localStorage.setItem("quizes_usuarios", JSON.stringify([res.data.id]))
-        }
-        
-    })
-    
-}
-
-/*função para enviar o quiz pro servidor */
-
-function atualizar_seus_quizes(){
-    let minha_galeria = document.querySelector(".minhaGaleria")
-    const quizes_usuarios = JSON.parse(localStorage.getItem("quizes_usuarios"))
-    for(let i = 0; i < quizes_usuarios.length; i++){
-        //console.log(quizes_usuarios[i])
-        const promessa = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${quizes_usuarios[i]}`)
-        promessa.then((res)=>{
-            // console.log(`id:${quizes_usuarios[i]}`)
-            // console.log(res.data.title)
-            // console.log(res.data.image)
-            // console.log("****************************")
-            minha_galeria.innerHTML += `
-            <div class="meuQuiz meuQuiz${i + 1}">
-                <img src="${res.data.image}"/>
-                <p>${res.data.title}</p>
-            </div>
-            `
-        })
-    }
-}
-
-atualizar_seus_quizes()
 /****************************************************************************************************/
 
 //scripts israel a baixo
@@ -314,7 +217,7 @@ function  renderizaPergunta(){
                     placeholder="Resposta Correta"
                 />
                 <input
-                    class="urlDaImagem"
+                    class="urlDaImagemCorreta"
                     type="text"
                     placeholder="URL da imagem"
                 />
@@ -355,74 +258,20 @@ function  renderizaPergunta(){
             </div>
             <!--Fecha caixa Respostas Incorretas-->
         </div>
-        <!--Fecha caixa pergunta 1-->
-
-        <div class="respostaCorreta">
-            <h1>Resposta Correta</h1>
-            <input
-                class="RespostaCorreta"
-                type="text"
-                placeholder="Resposta Correta"
-            />
-            <input
-                class="urlDaImagemCorreta"
-                type="text"
-                placeholder="URL da imagem"
-            />
-        </div>
-        <!--Fecha caixa Resposta Correta-->
-        <div class="respostasIncorretas">
-            <h1>Respostas incorretas</h1>
-            <input
-                class="respostaIncorreta"
-                type="text"
-                placeholder="Resposta incorreta 1"
-            />
-            <input
-                class="urlDaImagem"
-                type="text"
-                placeholder="URL da imagem 1"
-            />
-            <input
-                class="respostaIncorreta"
-                type="text"
-                placeholder="Resposta incorreta 2"
-            />
-            <input
-                class="urlDaImagem"
-                type="text"
-                placeholder="URL da imagem 2"
-            />
-            <input
-                class="respostaIncorreta"
-                type="text"
-                placeholder="Resposta incorreta 3"
-            />
-            <input
-                class="urlDaImagem"
-                type="text"
-                placeholder="URL da imagem 3"
-            />
-        </div>
-        <!--Fecha caixa Respostas Incorretas-->
-    </div>
-    <!--Fecha caixa caixa 1-->
-                   `
+    </div>`
         document.querySelector('.perguntas').innerHTML += template_caixaPergunta
     }
 }
  //botao 1 ativa redenriza perguntas       
  botao_criar_pergunta.addEventListener("click", renderizaPergunta)
 
-function renderiza(){
-    renderizaPergunta()
-    renderizaNivel() 
-}
-
-// função cria quantidade de niveis passadas 
-function renderizaNivel(elemento) {
-    const quantidadeNivel = document.querySelector('.nivelQuiz').value
+//funcao renderiza niveis
+function renderizaNivel() {
+    document.querySelector('.segundaParte').classList.add('desligado')
+    document.querySelector('.terceiraParte').classList.remove('desligado')
     document.querySelector('.niveis').innerHTML = ""
+    console.log("passou pro render nivel")
+    console.log(quantidadeNivel)
     for(let i = 0; i < quantidadeNivel; i++){   
        newNivel = i + 1
        template_caixaNivel = ` 
@@ -464,10 +313,14 @@ botao_criar_niveis.addEventListener("click", renderizaNivel)
 
 
 
-let botao_finalizar_quizz = document.querySelector(".terceiraParte .botaoNivel")
+// let botao_finalizar_quizz = document.querySelector(".terceiraParte .botaoNivel")
+//tratamento de paramentros 
 
+/*
 function info_basica_Nivel() {
-    //console.log(quantidadeNivel)
+    document.querySelector('.terceiraParte').classList.add('desligado')
+    document.querySelector('.quartaParte').classList.remove('desligado')
+    console.log(quantidadeNivel)
     titulo = document.querySelectorAll(".terceiraParte .tituloNivel")
     url = document.querySelectorAll(".terceiraParte .urlNivel")
     descricaoNivel = document.querySelectorAll(".terceiraParte .descricaoNivel")
@@ -483,12 +336,9 @@ function info_basica_Nivel() {
             alert("A URL da imagem esta incorreta")
             }
     }
-    enviaQuizz()
-}
-
-
-
-botao_finalizar_quizz.addEventListener("click", info_basica_Nivel)
+          
+}*/
+/*botao_finalizar_quizz.addEventListener("click", info_basica_Nivel)*/
    
 
 function voltar_home(){
